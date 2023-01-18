0535b5b6955734b7a0272fddcc286f10fa87a9fa
asutakaoutlook <122952127+asutakaoutlook@users.noreply.github.com>
2023-01-18 10:30:30 +0700

    Update server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -244,7 +244,7 @@ new SocketClient('fetusdt', 29, 3740000);
 new SocketClient('fidausdt', 30, 3745000); 
 new SocketClient('filusdt', 31, 3750000); 
 new SocketClient('fiousdt', 32, 3755000); 
-new SocketClient('firousdt', 33, 3760000); 
+//new SocketClient('firousdt', 33, 3760000); 
 new SocketClient('fisusdt', 34, 3765000); 
 new SocketClient('flmusdt', 35, 3770000); 
 new SocketClient('flowusdt', 36, 3775000);
